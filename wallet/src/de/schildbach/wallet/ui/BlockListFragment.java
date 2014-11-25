976f4dbefa47ec1fd3beb12ce8df594b4c267304
HashEngineering <hashengineeringsolutions@gmail.com>
2014-11-24 22:50:30 -0800

    v1.03 Updated block explorer and peer list and market graphics

--- wallet/src/de/schildbach/wallet/ui/BlockListFragment.java
+++ wallet/src/de/schildbach/wallet/ui/BlockListFragment.java
@@ -181,7 +181,7 @@ public final class BlockListFragment extends SherlockListFragment
 				switch (item.getItemId())
 				{
 					case R.id.blocks_context_browse:
-						startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.EXPLORE_BASE_URL + "block/"
+						startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.EXPLORE_BASE_URL + "Blocks/"
 								+ storedBlock.getHeight()/*getHeader().getHashAsString()*/)));
 
 						mode.finish();
